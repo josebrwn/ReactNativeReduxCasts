3cbb064bbeb3003756337f144731a91a44e168ab
Joseph Brown <joe@sendlove.io>
2016-12-29 14:37:20 -0700

    added flex:1 for scrollview

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,13 +1,17 @@
-// Index.ios.js - place code in here for IOS!!!!
+// Index.android.js - place code in here for Android!!!!
 
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <Header />
+  <View style={{ flex: 1 }}>
+    <Header headerText="Albums" />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
